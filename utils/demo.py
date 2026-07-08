a37f466a4b25f36c00ea7660179df6fdaed6037c
Amir Shaikh <amirshaikh1594@gmail.com>
2026-07-08 19:42:44 +0530

    Remove unnecessary code

--- utils/demo.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from youtube import transcript_download
-
-video_id = st.text_input('Enter video id')
-
-if st.button("Download"):
-    text = transcript_download(video_id)
-    st.markdown(text)
